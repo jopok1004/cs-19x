0e38ab7158886a85fe07212ee1530715352b69b7
maxnchua@gmail.com <maxnchua@gmail.com@42c874e9-3eb6-4696-8f5e-ee11df741a6e>
2012-04-13 04:35:26 +0000

    Added docu comments

--- trunk/Combined2/src/app/combined2/Receiver3GListener.java
+++ trunk/Combined2/src/app/combined2/Receiver3GListener.java
@@ -10,17 +10,26 @@ import org.jivesoftware.smack.util.StringUtils;
 import android.text.format.Time;
 import android.util.Log;
 
+/**
+ * A listener for 3G messages received by the receiver
+ *
+ */
+
 public class Receiver3GListener implements PacketListener{
 	
 	private ReceiverActivity receiver;
-	//private String fileType;
-	//private int packetSize;
 	private long messCtr;
-	//private HashMap<Integer, String> packetList = new HashMap<Integer, String>();
 
 	public Receiver3GListener (ReceiverActivity receiver) {
 		this.receiver = receiver;
 	}
+	
+	/**
+	 * Function that gets all packets in a given message
+	 * 
+	 * @param body Message that contains the packets
+	 * @param time The current time
+	 */
 
 	public void getPackets(String body, Time time) {
 		int packetNum;
@@ -35,19 +44,17 @@ public class Receiver3GListener implements PacketListener{
 			receiver.al.put(packetNum, packetLine);
 			
 			time.setToNow();
-			/*if (packetList.size() == 1) {
-				receiver.setInitial(time.toMillis(true));
-			}
-			try {
-				receiver.getWriter().write(time.toString() + " : Message " + packetNum + " Received\n");
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}*/
+
 			Log.i("XMPPReceiver:Receiving", "Received Packet number " + packetNum + " with value " + packetLine);
 			Log.i("XMPPReceiver:Receiving", "Current Map size " + Integer.toString(receiver.al.size()));
-			//Log.i("XMPPReceiver:Receiving", "Goal Map size " + packetSize);
 		}
 	}
+	
+	/**
+	 * Function that processes every messages received
+	 * 
+	 * @param packet The message received
+	 */
 
 	public void processPacket(Packet packet) {
 		Time time = new Time();
@@ -62,17 +69,7 @@ public class Receiver3GListener implements PacketListener{
 		reply.setThread(message.getThread());
 
 		if (message.getBody().startsWith("%&start3G")) {
-			/*String s = message.getBody().substring(11);
 
-			String n = "";
-			int i;
-			for (i = 0; s.charAt(i) != ' '; i++) {
-				n = n + s.charAt(i);
-			}
-			packetSize = Integer.parseInt(n);
-			fileType = s.substring(i + 1);
-			Log.i("XMPPReceiver:Receiving", "Packet size = " + packetSize + " File type = " + fileType);
-			*/
 			setMessCtr(0);
 			reply.setBody("%&proceed");
 			Log.e("XMPPReceiver:Sending", "Sending text [" + reply.getBody() + "]");
